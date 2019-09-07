5cdcb0a80fa67d8f9c67c061f2b56f6caa7bdffb
Ralph Schmieder <rschmied@cisco.com>
2019-09-07 12:31:38 +0200

    initial commit
    - created / moved workshop to devnet_university inventory
    - cleaned up / deleted a bunch of stuff
    - allowed to install pyats/genie into the alpine container
    - fixed a syntax error in the ansible-pyats role

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,15 @@
 FROM alpine:3.10
 
-COPY requirements.txt /tmp/requirements.txt
-RUN echo "===> install GCC ****" && \
+RUN echo "===> install GCC... " && \
     apk add --no-cache gcc musl-dev make && \
     \
     \
-    echo "===> install Python ****" && \
+    echo "===> install Python..." && \
     apk add --no-cache python3 && \
     if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi && \
     \
     \
-    echo "**** install pip ****" && \
+    echo "===> install pip..." && \
     python3 -m ensurepip && \
     rm -r /usr/lib/python*/ensurepip && \
     pip3 install --no-cache --upgrade pip setuptools wheel && \
@@ -18,15 +17,21 @@ RUN echo "===> install GCC ****" && \
     \
     \
     echo "===> Installing dependancies..."  && \
-    apk --update add sshpass libffi-dev libxml2-dev libxslt-dev python3-dev openssl-dev openssh-keygen && \
-    \
-    \
-    echo "===> Installing PIP Requirements..."  && \
+    apk --update add sshpass libffi-dev libxml2-dev libxslt-dev python3-dev openssl-dev openssh-keygen
+
+COPY requirements.txt /tmp/requirements.txt
+RUN echo "===> Installing PIP Requirements..."  && \
     pip install -r /tmp/requirements.txt
 
-COPY files/simple_client-0.1.9b14-py3-none-any.whl /tmp/simple_client-0.1.9b14-py3-none-any.whl
+# https://github.com/pypa/pip/issues/3969#issuecomment-247381915
+COPY _manylinux.py /usr/lib/python3.7/site-packages/_manylinux.py
+
+RUN echo "===> Installing PyATS / Genie requirement..."  && \
+    pip install pyats[full]==19.8
+
+COPY files/simple_client-0.1.9b15-py3-none-any.whl /tmp/simple_client-0.1.9b15-py3-none-any.whl
 RUN echo "===> Installing VIRL Client..."  && \
-    pip install /tmp/simple_client-0.1.9b14-py3-none-any.whl
+    pip install /tmp/simple_client-0.1.9b15-py3-none-any.whl
 
 # Define working directory.
 ENV ANSIBLE_HOST_KEY_CHECKING false
--- roles/ansible-pyats/filter_plugins/genie.py
+++ roles/ansible-pyats/filter_plugins/genie.py
@@ -8,6 +8,7 @@ try:
     from genie.conf.base import Device, Testbed
     from genie.libs.parser.utils import get_parser
     from genie.utils.diff import Diff
+    from genie.utils.config import Config
     HAS_GENIE = True
 except ImportError:
     HAS_GENIE = False
@@ -50,7 +51,7 @@ class FilterModule(object):
         else:
             return None
 
-    def genie_diff(self, output1, output2):
+    def genie_config_diff(self, output1, output2, mode=None, exclude=None):
         if not PY3:
             raise AnsibleFilterError("Genie requires Python 3")
 
@@ -60,15 +61,51 @@ class FilterModule(object):
         if not HAS_PYATS:
             raise AnsibleFilterError("pyATS not found. Run 'pip install pyats'")
 
-        diff = Diff(output1, output2)
+        supported_mode = ['add', 'remove', 'modified', None]
+        if mode not in supported_mode:
+            raise AnsibleFilterError("Mode '%s' is not supported. Specify %s." % (mode, supported_mode) )
 
-        diff.findDiff()
+        config1 = Config(output1)
+        config1.tree()
+        dict1 = config1.config
 
-        return diff
+        config2 = Config(output2)
+        config2.tree()
+        dict2 = config2.config
+
+        dd = Diff(dict1, dict2, mode=mode, exclude=exclude)
+        dd.findDiff()
+        diff = str(dd)
+        diff_list = diff.split('\n')
+
+        return diff_list
+
+
+    def genie_parser_diff(self, output1, output2, mode=None, exclude=None):
+        if not PY3:
+            raise AnsibleFilterError("Genie requires Python 3")
+
+        if not HAS_GENIE:
+            raise AnsibleFilterError("Genie not found. Run 'pip install genie'")
+
+        if not HAS_PYATS:
+            raise AnsibleFilterError("pyATS not found. Run 'pip install pyats'")
+
+        supported_mode = ['add', 'remove', 'modified', None]
+        if mode not in supported_mode:
+            raise AnsibleFilterError("Mode '%s' is not supported. Specify %s." % (mode, supported_mode) )
+
+        dd = Diff(output1, output2, mode=mode, exclude=exclude)
+        dd.findDiff()
+        diff = str(dd)
+        diff_list = diff.split('\n')
+
+        return diff_list
 
 
     def filters(self):
         return {
             'genie_parser': self.genie_parser,
-            'genie_diff': self.genie_diff
+            'genie_config_diff': self.genie_config_diff,
+            'genie_parser_diff': self.genie_parser_diff
         }
